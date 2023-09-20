5b0b76d2bbea79eb53379fd16057e3b51bd52183
ririfka08 <118185896+ririfka08@users.noreply.github.com>
2023-09-20 23:56:21 +0700

    Update form_data.dart

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -9,12 +9,64 @@ class FormData extends StatefulWidget {
 }
 
 class _FormDataState extends State<FormData> {
+  final _namaController = TextEditingController();
+  final -nimController = TextEditingController();
+  final -tahunLahirController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Input Data"),
+        title: const Text("Data Form"),
+      ), //AppBar
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            _textboxNama(),
+            _textboxNim(),
+            _textboxTahunLahir(),
+            _tombolSimpan(),
+          ],
+        )
       ),
     );
+  }
+
+  _textboxNama(){
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nama"),
+      controller: _namaController,
+    );
+  }
+
+  _textboxNim(){
+    return TextField(
+      decoration: const InputDecoration(labelText: "NIM"),
+      controller: _nimController,
+    );
+  }
+
+  _textboxTahunLahir(){
+    return TextField(
+      decoration: const InputDecoration(labelText: "Tahun Lahir"),
+      controller: _tahunLahirController,
+    );
+  }
+
+  _tombolSimpan(){
+    return ElevatedButton(
+      onPressed: () {
+        String nama = _namaController.text;
+        String nim = _nimController.text;
+        int tahunSekarang = DateTime.now().year;
+        int usia = tahunSekarang - tahunLahir;
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context).tampil_data(
+            nama: nama,
+            nim: nim,
+            tahunLahir: tahunLahir,
+        )));
+      },
+      child: const Text('Simpan'));
   }
 }
